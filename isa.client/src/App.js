00640a4179708a8585ccf990b1544f05a6a5b17d
diogofaria73 <diogofaria073@gmail.com>
2020-07-23 23:18:58 -0300

    Teste 2

--- isa.client/src/App.js
+++ isa.client/src/App.js
@@ -7,7 +7,7 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from './services/history';
 import { store, persistor } from './store';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
