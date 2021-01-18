21908998b72d3dfd38caf3a94ada77932f0108d7
Flavio Costa <fladrade@gmail.com>
2021-01-17 22:28:04 -0200

    branch_flavio_004

--- isa.api/src/app/controllers/DashboardController.js
+++ isa.api/src/app/controllers/DashboardController.js
@@ -1,6 +1,8 @@
 import OperationalArea from '../models/OperationalArea';
 import EquipmentType from '../models/EquipmentType';
 import Equipment from '../models/Equipment';
+import Consumption from '../models/Consumption';
+import { QueryTypes } from 'sequelize';
 
 class DashboardController {
   // Exibição da tela inicial de Dashboard.
@@ -47,13 +49,23 @@ class DashboardController {
     const powers = [];
     const costs = [];
 
-    const equipmentList = await Equipment.findAll({
-      order: [['tag', 'asc']],
-    });
-    equipmentList.forEach(eqp => {
-      labels.push([eqp.tag]);
-      powers.push(Math.random() * 100);
-      costs.push(Math.random() * 50);
+    // Query de exmeplo para consultar o consumo de cada equipamento por área ...
+    const resultQuery = await Consumption.sequelize.query(
+      // eslint-disable-next-line no-multi-str
+      'select co.plc_tag as tag, sum(co.consumption_value) as consumo \
+       from consumptions as co, equipment as eqp \
+       where co.plc_tag = eqp.tag and eqp.operational_area_id = :id_procurado \
+       group by co.plc_tag order by co.plc_tag',
+      {
+        replacements: { id_procurado: 1 },
+        type: QueryTypes.SELECT,
+      }
+    );
+
+    resultQuery.forEach(r => {
+      labels.push([r.tag]);
+      powers.push(r.consumo);
+      costs.push(r.consumo * Math.random() * 5);
     });
 
     return res.status(200).json({
