9bf1c972824c2623b21218c87eb72abd9d1e5dde
Flavio Costa <fladrade@gmail.com>
2021-01-25 21:12:22 -0200

    branch_flavio_004

--- isa.client/src/pages/Dashboard/graph.js
+++ isa.client/src/pages/Dashboard/graph.js
@@ -1,23 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import Chart from 'react-google-charts';
-import { Line, Bar, Doughnut } from 'react-chartjs-2';
+import { Line, Bar, Pie, HorizontalBar } from 'react-chartjs-2';
 import api from '../../services/api';
 
 export default function Graph() {
-  const [power, setPower] = useState([]);
-  const [powerJs, setPowerJs] = useState([]);
-
+  const [chartLine, setChartLine] = useState([]);
+  const [chartPie, setChartPie] = useState([]);
+  const [chartBar, setChartBar] = useState([]);
+  const [chartBarHoriz, setChartBarHoriz] = useState([]);
   useEffect(() => {
-    // Chamada para preencher o gráfico na app do google. Não funciona sem internet;
-    async function loadPowerData() {
-      const response = await api.post('dashboard/getPowerData');
-      const data = response.data.powerData;
-      setPower(data);
-    }
     // Chamada para preencher os gráficos quando a página é carregada;
     async function loadDataChartsJs() {
       const response = await api.post('dashboard/getDataChartJs');
-      const dataChartJs = {
+      const dataChartLine = {
         labels: response.data.labels,
         datasets: [
           {
@@ -36,9 +30,61 @@ export default function Graph() {
           },
         ],
       };
-      setPowerJs(dataChartJs);
+      const dataChartBar = {
+        labels: response.data.labels,
+        datasets: [
+          {
+            label: 'Potência Consumida',
+            data: response.data.powers,
+            fill: true,
+            backgroundColor: '#36A2EB',
+            borderColor: '#36A2EB',
+          },
+          {
+            label: 'Custo',
+            data: response.data.costs,
+            fill: true,
+            backgroundColor: '#FF6A33',
+            borderColor: '#FF6A33',
+          },
+        ],
+      };
+      const dataChartBarHoriz = {
+        labels: response.data.labels,
+        datasets: [
+          {
+            label: 'Potência Consumida',
+            data: response.data.powers,
+            fill: true,
+            backgroundColor: 'rgba(75,192,192,0.5)',
+            borderColor: 'rgba(75,192,192,1)',
+          },
+          {
+            label: 'Custo',
+            data: response.data.costs,
+            fill: true,
+            backgroundColor: 'rgba(45,15,45,0.5)',
+            borderColor: '#742774',
+          },
+        ],
+      };
+      const dataChartPie = {
+        labels: response.data.labels,
+        datasets: [
+          {
+            label: 'Potência Consumida',
+            data: response.data.powers,
+            fill: true,
+            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+            houverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+          },
+        ],
+      };
+      setChartLine(dataChartLine);
+      setChartBar(dataChartBar);
+      setChartPie(dataChartPie);
+      setChartBarHoriz(dataChartBarHoriz);
     }
-    loadPowerData();
     loadDataChartsJs();
   }, []);
 
@@ -46,42 +92,22 @@ export default function Graph() {
     <>
       <div className="row mt-5">
         <div className="col">
-          <h6 className="text-center">Potência e Custo Por Equipamento</h6>
-          <div style={{ display: 'flex', maxWidth: 900 }}>
-            <Chart
-              width={500}
-              height={400}
-              chartType="ColumnChart"
-              loader={<div>Loading Chart</div>}
-              data={power}
-              options={{
-                title: '',
-                chartArea: { width: '70%' },
-                hAxis: {
-                  title: 'Equipamentos',
-                  minValue: 0,
-                },
-                vAxis: {
-                  title: 'Valor',
-                },
-              }}
-              legendToggle
-            />
-          </div>
+          <h6 className="text-center">Potência - Bar ChartJs</h6>
+          <Bar data={chartBar} />
         </div>
         <div className="col">
           <h6 className="text-center">Potência - Line ChartJs</h6>
-          <Line data={powerJs} />
+          <Line data={chartLine} />
         </div>
       </div>
-      <div className="row">
+      <div className="row mt-5">
         <div className="col">
-          <h6 className="text-center">Potência - Bar ChartJs</h6>
-          <Bar data={powerJs} />
+          <h6 className="text-center">Potência - Bar Horizontal</h6>
+          <HorizontalBar data={chartBarHoriz} />
         </div>
         <div className="col">
-          <h6 className="text-center">Potência - Doughnut ChartJs</h6>
-          <Doughnut data={powerJs} />
+          <h6 className="text-center">Potência - Pizza ChartJs</h6>
+          <Pie data={chartPie} />
         </div>
       </div>
     </>
